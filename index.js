1071c703317bfdf5ef7001d92a9f968688dfd579
shreykapadia <shreykapadia@gmail.com>
2017-09-17 23:55:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
+var uppercase = "HELLO!"
+var lowercase = 'hello!'
   if(string === lowercase){
       return "I can\'t hear you!"
   }
